138adb89113642dca20b08399e41ace20a03a720
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2017-09-12 14:56:20 -0400

    Added declarations

--- src/intro/CodingConventions.java
+++ src/intro/CodingConventions.java
@@ -2,14 +2,21 @@ package intro;
 
 public class CodingConventions 
 {
+	// fields are at the top
+	// forward declarations
+	private String name;
+	private String description;
+	
 	// constructor, so the return type is an instance of the class
 	// a constructor must always be named after the class
 	public CodingConventions()
 	{
-		
+		name = "Benjamin";
+		description = "student";
 	}
 	public void doStuff()
 	{
-		
+		// static method call
+		System.out.println("123");
 	}
 }
--- src/intro/IntroMain.java
+++ src/intro/IntroMain.java
@@ -1,9 +1,20 @@
-package intro; // package declaration
+// package declaration
+package intro;
 
 public class IntroMain 
 {
 	public static void main(String[] args)
 	{
+		// declaration of an instance of CodingConventions
+		// also being instantiated
+		// always use new when calling constructors
+		CodingConventions conventionsInstance = new CodingConventions();
 		
+		// local variables are not fields
+		// they are variables that are only used in the scope of a method
+		// after the method they are destroyed
+		
+		// instance method call
+		conventionsInstance.doStuff();
 	}
 }
