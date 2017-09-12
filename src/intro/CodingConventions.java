1c467b7e130d5669bb80c20f5347a0f3410e501c
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2017-09-12 14:34:59 -0400

    Constructor

--- src/intro/CodingConventions.java
+++ src/intro/CodingConventions.java
@@ -2,7 +2,8 @@ package intro;
 
 public class CodingConventions 
 {
-	
+	// constructor, so the return type is an instance of the class
+	// a constructor must always be named after the class
 	public CodingConventions()
 	{
 		
