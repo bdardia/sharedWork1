f8850c2f2ca9d064afd76c1772fda62df78f45c7
bdardia <31863287+bdardia@users.noreply.github.com>
2017-09-12 19:18:10 -0400

    Update CodingConventions.java

--- src/intro/CodingConventions.java
+++ src/intro/CodingConventions.java
@@ -4,23 +4,26 @@ public class CodingConventions
 {
 	// fields are at the top
 	// forward declarations
-	private String name;
+	private String[] name;
 	private String description;
 	
 	// constructor, so the return type is an instance of the class
 	// a constructor must always be named after the class
-	public CodingConventions(String name, int descriptionIndex)
+	public CodingConventions(String[] name, int descriptionIndex)
 	{
 		// distinguish between parameters and other variables by using "this"
-		
-		this.name = name;
+		for (int i = 0; i < name.length; i += 1)
+		{
+			this.name[i] = name[i];	
+		}
 		description = " is a " + IntroMain.DESCRIPTIONS[descriptionIndex];
 	}
 	public void doStuff()
 	{
-		String output = name + description;
-		
-		// static method call
-		System.out.println(output);
+		for (int i = 0; i < name.length; i += 1)
+		{
+			String output = name[i] + description;
+			System.out.println(output);
+		}
 	}
 }
