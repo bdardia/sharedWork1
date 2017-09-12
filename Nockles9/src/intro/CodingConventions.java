59ba840e59551bd33461e927445d721377f3b08b
Teacher <Teacher@10.8.39.80>
2017-09-12 14:51:16 -0400

    created coding conventions

---
+++ Nockles9/src/intro/CodingConventions.java
@@ -0,0 +1,20 @@
+package intro;
+
+public class CodingConventions {
+
+	
+	/*
+	 * this is a constructor, it is like a method except the "return type" is an instance of the class
+	 * for this reason, a constructor must ALWAYS be named after the class
+	 */
+	public CodingConventions(){
+		
+	}
+	
+	//normal method "void" is the return type
+	public void doStuff(){
+		//static method call:
+		
+		System.out.println("Hello world!");
+	}
+}
--- Nockles9/src/intro/IntroMain.java
+++ Nockles9/src/intro/IntroMain.java
@@ -1,6 +1,23 @@
-package intro;
+package intro;//package declaration: matches folder where this file is located
 
+//class header
 public class IntroMain {
-//line of text
-  //another line of text
+
+	//this is the first method that is executed. It is static because it is independent of instances
+	public static void main(String[] args){
+		//this is the declaration of an instance of CodingConventions
+		//in the same line of code, it is also being instantiated
+		//note the use of the word "new" this word is always used when calling constructors
+		CodingConventions conventionsInstance = new CodingConventions();
+		//a note on local variables: they are NOT FIELDS (HAS-A relationships)
+		//they are variables that are only used in the scope of a method
+		//after the method, they are destroyed
+		
+		/*
+		 * instance method call (the method belongs to the instance, not the class)
+		 */
+		conventionsInstance.doStuff();
+	}
+	
+	
 }
