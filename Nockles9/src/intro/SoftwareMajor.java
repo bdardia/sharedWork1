6d0126310af3e8654635fd125b9e1049223e7402
Teacher <Teacher@10.8.39.80>
2017-09-15 15:13:04 -0400

    Finished Duel and Dueler

---
+++ Nockles9/src/intro/SoftwareMajor.java
@@ -0,0 +1,19 @@
+package intro;
+
+public class SoftwareMajor extends Student implements SymposiumPresenter{
+
+	private String name;\
+	
+	@Override
+	public void present() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getPresentationName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
